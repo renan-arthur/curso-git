4fffc1d22ee505e8b69ddaabe0a38ba9f493613f
Renan Arthur <renan.albino@fitbank.com.br>
2021-06-18 08:43:02 -0300

    Adicionada descrição do Gitt

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -8,6 +8,14 @@ namespace projeto_teste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+
+            //Comandos Git
+
+            //git init - cria diretório
+            //git add . - utilizado para comittar os arquivos e pastas
+            //git status - Mostra todos os arquivos comittados e não comittados
+            //git commit -m "Mensagem" - Serve para descrever o q aconteceu na committação
+            //git log - mostra o q foi commitado, quem comittou e o que comittou
         }
     }
 }
