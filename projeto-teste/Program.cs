fd315b9a5572c7ea83b983cc1b2ecc661b2d86a6
Renan Arthur <renan.albino@fitbank.com.br>
2021-06-18 09:13:42 -0300

    Acrescentado impressão de 'Teste' na tela

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -12,10 +12,16 @@ namespace projeto_teste
             //Comandos Git
 
             //git init - cria diretório
-            //git add . - utilizado para comittar os arquivos e pastas
-            //git status - Mostra todos os arquivos comittados e não comittados
+            //git add . - utilizado para commitar os arquivos e pastas
+            //git status - Mostra todos os arquivos commitados e não comittados
             //git commit -m "Mensagem" - Serve para descrever o q aconteceu na committação
-            //git log - mostra o q foi commitado, quem comittou e o que comittou
+            //git log - mostra o q foi commitado, quem commitou e o que commitou
+
+            //Se esquecer da mensagem no committ, o Git Bash abre um editor VIM.
+            //Para sair do VIM tecle <ESC> ou <ENTER>, ou digite ":q!".
+
+            Console.WriteLine("Teste");
+
         }
     }
 }
