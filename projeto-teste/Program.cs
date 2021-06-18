160c1135a9f1c76c56aa563adf3d1b853daa6872
Renan Arthur <renan.albino@fitbank.com.br>
2021-06-18 09:24:23 -0300

    modificando 'Bom dia' para 'Boa tarde'.

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -7,10 +7,9 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
+            Console.WriteLine("Boa tarde");
 
             //Comandos Git
-
             //git init - cria diretório
             //git add . - utilizado para commitar os arquivos e pastas
             //git status - Mostra todos os arquivos commitados e não comittados
@@ -19,6 +18,9 @@ namespace projeto_teste
 
             //Se esquecer da mensagem no committ, o Git Bash abre um editor VIM.
             //Para sair do VIM tecle <ESC> ou <ENTER>, ou digite ":q!".
+
+            //git reset --soft HEAD~1 |Remove o último commit mantendo as alterações nos arquivos.
+            //git reset --hard HEAD~1 |Remove o último commit INCLUSIVE as alterações nos arquivos (PERIGO!).
 
             Console.WriteLine("Teste");
 
